8250436de8150517a385242d40b9a162417ca6d4
Robbie Buxton <138501839+rbpdt@users.noreply.github.com>
2023-08-16 17:29:12 +0100

    minor refactor removing unnecessary flags

--- script/rootless/run-redis.sh
+++ script/rootless/run-redis.sh
@@ -3,5 +3,5 @@ UID=$(id -u)
 
 export CONTAINERD_SNAPSHOTTER=nix 
 export CONTAINERD_ADDRESS=/run/user/$UID/containerd/containerd.sock
-nsenter -U --preserve-credentials -m -n -t $(cat /run/user/$UID/rootlesskit-containerd/child_pid) nerdctl -n k8s.io pull docker.io/library/redis:alpine
-nsenter -U --preserve-credentials -m -n -t $(cat /run/user/$UID/rootlesskit-containerd/child_pid) ctr -n k8s.io run -t --rm --cgroup "" docker.io/library/redis:alpine redis
+nsenter -U --preserve-credentials -m -n -t $(cat /run/user/$UID/rootlesskit-containerd/child_pid) nerdctl pull docker.io/library/redis:alpine
+nsenter -U --preserve-credentials -m -n -t $(cat /run/user/$UID/rootlesskit-containerd/child_pid) ctr run -rm --cgroup "" docker.io/library/redis:alpine redis
