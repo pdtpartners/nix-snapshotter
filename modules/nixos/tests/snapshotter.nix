b16386345e74bbc151148952d10429d5c74ca072
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2023-08-25 12:30:25 -0700

    Add rootless containerd and nix-snapshotter nixos modules
    - Attribute modified code from nerdctl in NOTICE
    - Modify nixos/tests/snapshotter.nix to separate rootful and rootless tests

--- modules/nixos/tests/snapshotter.nix
+++ modules/nixos/tests/snapshotter.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   registryHost = "127.0.0.1";
 
@@ -5,20 +6,27 @@ let
 
   imageName = "${registryHost}:${toString registryPort}/hello";
 
-in {
-  nodes.machine = { pkgs, nix-snapshotter-parts, ... }:
+  regularTag = "latest";
+  regularImage = "${imageName}:${regularTag}";
+
+  nixTag = "nix";
+  nixImage = "${imageName}:${nixTag}";
+
+  base = { pkgs, nix-snapshotter-parts, ... }:
     let
-      hello = nix-snapshotter-parts.buildImage {
+      helloTarball = pkgs.dockerTools.buildImage {
         name = imageName;
-        tag = "latest";
+        tag = regularTag;
+        config.entrypoint = ["${pkgs.hello}/bin/hello"];
+      };
+
+      hello-nix = nix-snapshotter-parts.buildImage {
+        name = imageName;
+        tag = nixTag;
         config.entrypoint = ["${pkgs.hello}/bin/hello"];
       };
 
     in {
-      imports = [ ../nix-snapshotter.nix ];
-
-      services.nix-snapshotter.enable = true;
-
       # Setup local registry for testing `buildImage` and `copyToRegistry`.
       services.dockerRegistry = {
         enable = true;
@@ -26,25 +34,113 @@ in {
         port = registryPort;
       };
 
+      environment.variables = {
+        HELLO_TARBALL = helloTarball;
+      };
+
       environment.systemPackages = [
-        (hello.copyToRegistry { plainHTTP = true; })
+        (hello-nix.copyToRegistry { plainHTTP = true; })
         pkgs.nerdctl
       ];
     };
 
-  testScript = ''
-    machine.start(allow_reboot = True)
+  rootful = {
+    imports = [ base ../nix-snapshotter.nix ];
 
-    machine.wait_for_unit("nix-snapshotter.service")
-    machine.wait_for_unit("containerd.service")
+    services.nix-snapshotter.enable = true;
+  };
 
-    machine.wait_for_unit("docker-registry.service")
-    machine.wait_for_open_port(${toString registryPort})
+  rootless = {
+    imports = [ base ../nix-snapshotter-rootless.nix ];
 
-    with subtest("Nix-snapshotter image copied to local registry"):
+    services.nix-snapshotter.rootless.enable = true;
+
+    users.users.alice = {
+      uid = 1000;
+      isNormalUser = true;
+    };
+  };
+
+  both = { imports = [ rootful rootless ]; };
+
+in {
+  nodes = { inherit rootful rootless both; };
+
+  testScript = { nodes, ... }:
+    let
+      user = nodes.rootless.users.users.alice;
+
+      sudo_su = lib.concatStringsSep " " [
+        "XDG_RUNTIME_DIR=/run/user/${toString user.uid}"
+        "sudo_su" "--preserve-env=XDG_RUNTIME_DIR" "-u" "alice"
+      ];
+
+    in ''
+      def setup(machine):
+        machine.wait_for_unit("docker-registry.service")
+        machine.wait_for_open_port(${toString registryPort})
         machine.succeed("copy-to-registry")
 
-    with subtest("Nerdctl can pull and run nix-snapshotter image"):
-        assert "Hello, world!" in machine.succeed("nerdctl --snapshotter nix run ${imageName}")
-  '';
+      def wait_for_user_unit(machine, service, user = "alice"):
+        machine.wait_until_succeeds(f"systemctl --user --machine={user}@ is-active {service}")
+
+      def test_rootful(machine, name = "rootful"):
+        machine.wait_for_unit("nix-snapshotter.service")
+        machine.wait_for_unit("containerd.service")
+
+        with subtest(f"{name}: Run regular container as root"):
+          machine.succeed("nerdctl --snapshotter nix load < $HELLO_TARBALL")
+          out = machine.succeed("nerdctl --snapshotter nix run --name hello ${regularImage}")
+          assert "Hello, world!" in out
+          machine.succeed("nerdctl ps -a | grep hello")
+          machine.succeed("nerdctl rm hello")
+
+        with subtest(f"{name}: Run nix container as root"):
+          out = machine.succeed("nerdctl --snapshotter nix run --name hello ${nixImage}")
+          assert "Hello, world!" in out
+          machine.succeed("nerdctl ps -a | grep hello")
+          machine.succeed("nerdctl rm hello")
+
+      def test_rootless(machine, name = "rootless"):
+        machine.succeed("loginctl enable-linger alice")
+        wait_for_user_unit(machine, "nix-snapshotter.service")
+        wait_for_user_unit(machine, "containerd.service")
+
+        with subtest(f"{name}: Run regular container as user"):
+          machine.succeed("${sudo_su} nerdctl --snapshotter nix load < $HELLO_TARBALL")
+          out = machine.succeed("${sudo_su} nerdctl --snapshotter nix run --name hello ${regularImage}")
+          assert "Hello, world!" in out
+          machine.succeed("${sudo_su} nerdctl ps -a | grep hello")
+          machine.succeed("${sudo_su} nerdctl rm hello")
+
+        # TODO: Currently rootless nerdctl cannot pull images from 127.0.0.1,
+        # because the pull operation occurs in rootlesskit's network namespace.
+        #
+        # With upcoming rootlesskit v2.0.0, there is a new flag `--detach-netns`
+        # that when enabled, mounts the new netns to $ROOTLESSKIT_STATE_DIR/netns
+        # and launches slirp4netns for that netns, but leaves containerd in host netns
+        # with unshared userns and mountns.
+        #
+        # See:
+        # - https://github.com/containerd/nerdctl/blob/main/docs/registry.md#accessing-127001-from-rootless-nerdctl
+        # - https://github.com/containerd/nerdctl/issues/814
+        # - https://github.com/rootless-containers/rootlesskit/pull/379
+        # with subtest(f"{name}: Run nix container as user"):
+        #   out = machine.succeed("${sudo_su} nerdctl --snapshotter nix run --name hello ${nixImage}")
+        #   assert "Hello, world!" in out
+        #   machine.succeed("${sudo_su} nerdctl ps -a | grep hello")
+        #   machine.succeed("${sudo_su} nerdctl rm hello")
+
+      start_all()
+
+      setup(rootful)
+      test_rootful(rootful)
+
+      setup(rootless)
+      test_rootless(rootless)
+
+      setup(both)
+      test_rootful(both, "both")
+      test_rootless(both, "both")
+    '';
 }
