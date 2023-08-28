07600b495577d7b6645064058e9e8fd4d6270486
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2023-08-27 20:02:53 -0400

    Add missing WithNixStoreDir option to main.go (#54)

--- main.go
+++ main.go
@@ -121,7 +121,7 @@ func serve(ctx context.Context, cfg *config.Config) error {
 		return fmt.Errorf("failed to remove %q: %w", cfg.Address, err)
 	}
 
-	sn, err := nix.NewSnapshotter(cfg.Root, "/nix/store")
+	sn, err := nix.NewSnapshotter(cfg.Root, nix.WithNixStoreDir("/nix/store"))
 	if err != nil {
 		return err
 	}
