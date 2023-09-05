adf57e6596c936826c007f659e0ea92ffa0028cc
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2023-09-05 01:43:17 -0700

    Update rootless scripts to fix regression in main (#68)

--- script/rootless/common.sh
+++ script/rootless/common.sh
@@ -1,7 +1,9 @@
 export REPO_DIR=$(git rev-parse --show-toplevel)
-export CONTAINERD_SNAPSHOTTER="nix"
+
+export BIN_DIR="${REPO_DIR}/build/bin"
 export CONTAINERD_ADDRESS="${REPO_DIR}/build/containerd/containerd.sock"
 export CONTAINERD_CONFIG_FILE="${REPO_DIR}/build/containerd/config.toml"
+export CONTAINERD_SNAPSHOTTER="nix"
 export NERDCTL_TOML="${REPO_DIR}/build/nerdctl/nerdctl.toml"
 export NIX_SNAPSHOTTER_CONFIG_FILE="${REPO_DIR}/build/nix-snapshotter/nix-snapshotter.toml"
 export ROOTLESSKIT_STATE_DIR=$REPO_DIR/build/rootlesskit-containerd/
