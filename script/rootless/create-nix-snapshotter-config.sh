6225bc8f0e99b999b1d97eff205427e87b484ecc
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2023-09-05 01:26:44 -0700

    Update rootless scripts to fix regression in main

--- script/rootless/create-nix-snapshotter-config.sh
+++ script/rootless/create-nix-snapshotter-config.sh
@@ -6,4 +6,7 @@ mkdir -p $(dirname $NIX_SNAPSHOTTER_CONFIG_FILE)
 cat <<EOM > $NIX_SNAPSHOTTER_CONFIG_FILE
 address = "${REPO_DIR}/build/nix-snapshotter/nix-snapshotter.sock"
 root    = "${REPO_DIR}/build/containerd/root/io.containerd.snapshotter.v1.nix"
+
+[image_service]
+containerd_address = "${REPO_DIR}/build/containerd/containerd.sock"
 EOM
