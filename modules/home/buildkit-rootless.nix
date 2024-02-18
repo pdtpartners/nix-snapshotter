ef8ec3b8bad53935d6230f2a6e0c145b07d874d0
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2024-02-18 16:33:05 +0800

    Add buildkit-rootless home module

---
+++ modules/home/buildkit-rootless.nix
@@ -0,0 +1,101 @@
+{ config, pkgs, lib, ... }:
+let
+  inherit (lib)
+    mkEnableOption
+    mkOption
+    mkPackageOption
+    types
+  ;
+
+  inherit (config.virtualisation.containerd.rootless)
+    nsenter
+  ;
+
+  cfg = config.services.buildkit.rootless;
+
+  settingsFormat = pkgs.formats.toml {};
+
+  buildkitService = {
+    Unit = {
+      Description = "BuildKit - a toolkit for converting source code to build artifacts (Rootless)";
+      After = [ "containerd.service" ];
+      PartOf = [ "containerd.service" ];
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      KillMode = "mixed";
+      Restart = "always";
+      RestartSec = "2";
+      Environment = "PATH=${lib.makeBinPath cfg.path}";
+
+      ExecStart = "${nsenter}/bin/containerd-nsenter ${cfg.package}/bin/buildkitd --rootless";
+
+      StateDirectory = "buildkit";
+      RuntimeDirectory = "buildkit";
+      RuntimeDirectoryPreserve = "yes";
+    };
+  };
+
+in {
+  imports = [
+    ../common/containerd-rootless.nix
+    ../common/nix-snapshotter-rootless.nix
+  ];
+
+  options.services.buildkit.rootless = {
+    enable = mkEnableOption (lib.mdDoc "buildkit");
+
+    configFile = mkOption {
+      type = types.nullOr types.path;
+      description = lib.mdDoc ''
+       Path to nix-snapshotter config file.
+       Setting this option will override any configuration applied by the
+       settings option.
+      '';
+    };
+
+    package = mkPackageOption pkgs "buildkit" { };
+
+    path = mkOption {
+      type = types.listOf types.path;
+      description = lib.mdDoc ''
+        Packages to be included in the PATH for k3s.
+      '';
+    };
+
+    settings = mkOption {
+      type = settingsFormat.type;
+      default = {};
+      description = lib.mdDoc ''
+        Verbatim lines to add to config.toml
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.buildkit.rootless = {
+      configFile =
+        lib.mkOptionDefault
+          (settingsFormat.generate "config.toml" cfg.settings);
+
+      path = with pkgs; [
+        git
+      ];
+
+      settings = {
+        worker.containerd = with config.virtualisation.containerd.rootless; {
+          enabled = true;
+          address = setAddress;
+          namespace = setNamespace;
+        };
+      };
+    };
+
+    systemd.user.services.buildkit = buildkitService;
+  };
+}
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -10,11 +10,15 @@
       imports = [
         nix-snapshotter-rootless
         containerd-rootless
+        preload-containerd-rootless
+        k3s-rootless
+        buildkit-rootless
       ];
     };
     nix-snapshotter-rootless = ./nix-snapshotter-rootless.nix;
     containerd-rootless = ./containerd-rootless.nix;
     preload-containerd-rootless = ./preload-containerd-rootless.nix;
     k3s-rootless = ./k3s-rootless.nix;
+    buildkit-rootless = ./buildkit-rootless.nix;
   };
 }
