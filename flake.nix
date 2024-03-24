3743eb2adcd7e849cbb119f64caa3e07787a91f8
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2024-03-24 09:32:21 +0800

    Bump nixpkgs to attempt to fix golangci-lint

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Containerd snapshotter that understands nix store paths natively.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
