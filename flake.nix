c10b066a4b1bb3451507c141636014e3335e579e
Yureka <yuka@yuka.dev>
2024-06-09 17:58:21 +0200

    add aarch64 to systems

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
 
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
       imports = [ ./modules ];
     };
 }
